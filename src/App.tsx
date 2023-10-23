68a25652c9c1071601e1cfdfaa605cbe7667b40f
Rava Maulana <ravamaulana14@gmail.com>
2023-10-23 14:34:46 +0700

    feat: connect to rest service

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,14 @@
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Box,
-} from "@chakra-ui/react";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import DummyPage from "./pages/dummy";
 
 export default function App() {
-  const [dummyData, setDummyData] = useState("");
+  const router = createBrowserRouter([
+    {
+      path: "/dummy",
+      element: <DummyPage />,
+    },
+  ]);
 
-  useEffect(() => {
-    (async () => {
-      const res = await axios.get("http://127.0.0.1:3000/dummy");
-      setDummyData(res.data.description)
-    })();
-  }, []);
-
-  return (
-    <>
-      <h1 className="text-green-400 text-7xl">HELLO WORLD</h1>
-
-      <Accordion>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                Section 1 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                Section 2 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-
-      <h2 className="text-6xl text-red-500">{dummyData}</h2>
-    </>
-  );
+  return <RouterProvider router={router} />
 }
---
+++ src/pages/dummy/index.tsx
@@ -0,0 +1,65 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function DummyPage() {
+  const [dummyData, setDummyData] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get("http://127.0.0.1:3000/dummy");
+      setDummyData(res.data.description)
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-green-400 text-7xl">HELLO WORLD</h1>
+
+      <Accordion>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Section 1 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Section 2 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+
+      <h2 className="text-6xl text-red-500">{dummyData}</h2>
+    </>
+  );
+}
