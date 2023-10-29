cd6efa2e95ac16a9874d292892f09e03cde4963e
Rava Maulana <ravamaulana14@gmail.com>
2023-10-29 20:40:43 +0700

    feat: added responsive home page

--- src/components/PodcastCard.tsx
+++ src/components/PodcastCard.tsx
@@ -10,22 +10,22 @@ export type cardProps = {
 
 export default function PodcastCard({title, description, imageurl}: cardProps): JSX.Element {
   return (
-    <div className="w-[200px] h-[288px] rounded-xl overflow-hidden border-NAVY-5 border-2 shadow-[-2px_2px_4px_0_#5C67DE,2px_-2px_4px_0_#5C67DE,-2px_-2px_4px_0_#5C67DE,2px_2px_4px_0_#5C67DE] hover:shadow-[-2px_2px_4px_0_#F5D049,2px_-2px_4px_0_#F5D049,-2px_-2px_4px_0_#F5D049,2px_2px_4px_0_#F5D049] hover:bg-YELLOW-5 hover:border-YELLOW-1 group shrink-0">
+    <div className="w-[160px] h-[230px] rounded-xl overflow-hidden border-NAVY-5 border-2 shadow-[-2px_2px_4px_0_#5C67DE,2px_-2px_4px_0_#5C67DE,-2px_-2px_4px_0_#5C67DE,2px_2px_4px_0_#5C67DE] hover:shadow-[-2px_2px_4px_0_#F5D049,2px_-2px_4px_0_#F5D049,-2px_-2px_4px_0_#F5D049,2px_2px_4px_0_#F5D049] hover:bg-YELLOW-5 hover:border-YELLOW-1 group shrink-0 xl:w-[200px] xl:h-[288px]">
       <img
-        className="w-[200px] h-[175px] object-cover object-center"
+        className="w-[160px] h-[140px] object-cover object-center xl:w-[200px] xl:h-[175px]"
         src={imageurl || Placeholder}
         width={200}
         height={175}
         alt="podcast-thumbnail"
       />
 
-      <div className="pt-4 pb-6 px-2.5 w-full group-hover:text-NAVY-2 relative">
+      <div className="pt-3 pb-5 px-1.5 w-full group-hover:text-NAVY-2 relative xl:pt-4 xl:pb-6 xl:px-2.5">
         <button className="invisible group-hover:visible flex absolute right-2.5 top-0 -translate-y-[10px] group-hover:-translate-y-[32px] items-center justify-center rounded-full bg-BLACK py-4 pl-[18px] pr-3.5 transition-transform duration-500">
           <img src={PlayIcon} width={16} height={16} alt="pause-episode" />
         </button>
 
-        <h4 className="h5 w-full">{title}</h4>
-        <p className="mt-2.5 text-[10px] w-full">
+        <h4 className="h5 w-full text-ellipsis whitespace-nowrap overflow-hidden md:max-xl:text-xs">{title}</h4>
+        <p className="mt-2.5 text-[10px] w-full md:max-xl:text-[8px]">
           {description}
         </p>
       </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -15,7 +15,7 @@ export default function HomePage(): JSX.Element {
       window.addEventListener("resize", () => {
         if (window.innerWidth >= 1500) {
           setCardCount(5)
-        } else if (window.innerWidth >= 1220) {
+        } else if (window.innerWidth >= 1024) {
           setCardCount(4)
         } else {
           setCardCount(3)
@@ -27,43 +27,43 @@ export default function HomePage(): JSX.Element {
   useEffect(() => {
     (async () => {
       const resTech = await axios.get(
-        "http://127.0.0.1:3000/podcast/random/technology"
+        "http://localhost:3000/podcast/random/technology"
       );
       setTechPodcasts(resTech.data.podcasts);
 
       const resComedy = await axios.get(
-        "http://127.0.0.1:3000/podcast/random/comedy"
+        "http://localhost:3000/podcast/random/comedy"
       );
       setComedyPodcasts(resComedy.data.podcasts);
 
       const resHorror = await axios.get(
-        "http://127.0.0.1:3000/podcast/random/horror"
+        "http://localhost:3000/podcast/random/horror"
       );
       setHorrorPodcasts(resHorror.data.podcasts);
     })();
   }, []);
 
   return (
-    <section className="px-8 pb-20">
-      <h2 className="h1">Technology</h2>
+    <section className="px-6 pb-10 xl:px-8 xl:pb-20">
+      <h2 className="h1 md:max-lg:text-xl lg:max-xl:text-2xl">Technology</h2>
 
-      <div className="mt-4 -ml-1.5 px-1.5 py-2 flex justify-between overflow-x-auto">
+      <div className="mt-2 -ml-1.5 px-1.5 py-2 flex justify-between overflow-x-auto xl:mt-4">
         {techPodcasts?.slice(0, cardCount).map((podcast: cardProps, idx: number) => (
           <PodcastCard key={idx} {...podcast} />
         ))}
       </div>
 
-      <h2 className="h1 mt-6">Comedy</h2>
+      <h2 className="h1 mt-4 md:max-lg:text-xl lg:max-xl:text-2xl xl:mt-6">Comedy</h2>
 
-      <div className="mt-4 -ml-1.5 px-1.5 py-2 flex justify-between overflow-x-auto">
+      <div className="mt-2 -ml-1.5 px-1.5 py-2 flex justify-between overflow-x-auto xl:mt-4">
         {comedyPodcasts?.slice(0, cardCount).map((podcast: cardProps, idx: number) => (
           <PodcastCard key={idx} {...podcast} />
         ))}
       </div>
 
-      <h2 className="h1 mt-6">Horror</h2>
+      <h2 className="h1 mt-4 md:max-lg:text-xl lg:max-xl:text-2xl xl:mt-6">Horror</h2>
 
-      <div className="mt-4 -ml-1.5 px-1.5 py-2 flex justify-between overflow-x-auto">
+      <div className="mt-2 -ml-1.5 px-1.5 py-2 flex justify-between overflow-x-auto xl:mt-4">
         {horrorPodcasts?.slice(0, cardCount).map((podcast: cardProps, idx: number) => (
           <PodcastCard key={idx} {...podcast} />
         ))}
